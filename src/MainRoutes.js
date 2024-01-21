5ab00be751239039303efdce556803051fa939d5
rysyaa <captain0demon@gmail.com>
2024-01-21 21:52:36 +0600

    conflickt

--- src/MainRoutes.js
+++ src/MainRoutes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Main from "./components/Main";
+import Person from "./components/Person";
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/person" element={<Person/>}/>
     </Routes>
   );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import Person from "./Person";
 
 const Main = () => {
   return (
